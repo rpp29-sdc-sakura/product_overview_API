dab1fba7fd572407034674ad02b4cb35ea194e8f
Ryan Lott <ryanclott@outlook.com>
2021-08-28 10:27:18 -0500

    Initial Server Setup Pt 2

--- server/index.js
+++ server/index.js
@@ -1,16 +1,11 @@
 const express = require('express');
 let app = express();
 
-app.use(express.static(__dirname + '/../client/dist'));
 app.use(express.urlencoded({
     extended: true
   })
 );
 
-
-app.get('', function (req, res) {
-
-});
 
 
 let port = 3000;
