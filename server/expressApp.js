30b9afc978f3ba2a978a09d53301c817a1a200d6
Ryan Lott <ryanclott@outlook.com>
2021-09-27 18:47:10 -0500

    Express definition split from listener

---
+++ server/expressApp.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const dbHelpers = require('./dbHelpers.js');
+
+let app = express();
+
+app.use(express.urlencoded({
+    extended: true
+  })
+);
+
+app.get('/products', (req, res) => {
+  dbHelpers.fetchProducts(req.query)
+  .then(result => {
+    res.status(200).send(result);
+  })
+});
+
+app.get('/products/:product_id', (req, res) => {
+  let productId = parseInt(req.params['product_id']);
+  if(isNaN(productId)) {
+    res.status(400).send('Error: invalid product id provided');
+  } else {
+    dbHelpers.fetchProduct(productId)
+    .then(result => {
+      res.status(200).send(result);
+    });
+  }
+});
+
+
+app.get('/products/:product_id/styles', (req, res) => {
+  let productId = parseInt(req.params['product_id']);
+  if(isNaN(productId)) {
+    res.status(400).send('Error: invalid product id provided');
+  } else {
+    dbHelpers.fetchProductStyles(productId)
+    .then(result => {
+      res.status(200).send(result);
+    });
+  }
+});
+
+module.exports = app;
